1cde9b16c00e63f46c11cb9c1eda63f4155fe465
Erickssom Melgaco de Oliveira <erickssommelgaco@gmail.com>
2024-06-28 14:44:34 -0300

    Exercicios 1 - 6

---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿///Exercicio 1
+
+// Console.WriteLine("Informe o salario");
+
+// double salario = double.Parse(Console.ReadLine());
+
+// double porcentagem = salario * 0.25;
+
+// salario = salario + porcentagem;
+
+// Console.WriteLine($"Novo salario {salario}"); 
+
+
+///Exercicio 2 
+
+// Console.WriteLine("Informe o salario");
+
+// double salario = double.Parse(Console.ReadLine());
+
+// Console.WriteLine("Informe a porcentagem para incrementar");
+
+// double porcentagem = double.Parse(Console.ReadLine());
+
+// double porcentagemReal = porcentagem / 100; 
+
+// double calculo = salario * porcentagemReal;
+
+// double novoSalario = salario + calculo;
+
+// Console.WriteLine($"Novo salario {novoSalario}");
+
+// /Exercicio 3 
+
+// Console.WriteLine("Ano de Nascimento");
+
+// int anoNascimento = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Ano Atual");
+
+// int anoAtual = int.Parse(Console.ReadLine());
+
+// int idadeAnos = anoAtual - anoNascimento; 
+
+// int idadeMeses = idadeAnos * 12; 
+
+// int idadeSemana = idadeAnos * 52;
+
+// int idadeDias = idadeAnos * 365;
+
+// Console.WriteLine($"Idades em Anos: {idadeAnos}\nIdade em Meses: {idadeMeses}\nIdade em Semanas: {idadeSemana}\nIdade Dias: {idadeDias}");
+
+
+///Exercicio 4 
+
+// Console.WriteLine("Peso do Saco:");//10kilos
+
+// double sacoRacao = double.Parse(Console.ReadLine());
+
+// double gramas = sacoRacao * 1000;
+
+// Console.WriteLine("Porção da raçao:");//200 gramas
+
+// double porcao = double.Parse(Console.ReadLine());
+
+// double consumoDiario = porcao * 2 ;
+
+// double consumo5dias = consumoDiario * 5; 
+
+// double racaoRestante = sacoRacao / consumo5dias;
+
+// Console.WriteLine($"Ração restante no saco {racaoRestante} ");
+
+///Exercicio 5 
+
+// Console.WriteLine("Coloque o valor de A");
+// int A = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Coloque o valor de B");
+// int B = int.Parse(Console.ReadLine());
+
+// int C = A;
+// A = B;
+// B = C;  
+// Console.WriteLine($"O valor de A é:{A}\nO valor de B é:{B}");
+
+///Exercicio 6
+
+// Console.WriteLine("Diga o Comprimento da caixa");
+// double comprimento = double.Parse(Console.ReadLine());  
+
+// Console.WriteLine("Diga a Largura da caixa");
+// double largura = double.Parse(Console.ReadLine()); 
+
+// Console.WriteLine("Diga a Altura da caixa");
+// double altura = double.Parse(Console.ReadLine());
+
+// double volume = comprimento * largura * altura;
+// Console.WriteLine($"O Volume da Caixa é {volume}");
+
+///Exercicio 7

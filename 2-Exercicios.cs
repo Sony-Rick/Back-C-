af0dbe13b43f193004ae3dc0968924514970035b
Erickssom Melgaco de Oliveira <erickssommelgaco@gmail.com>
2024-07-03 18:45:03 -0300

    Exercicio 1 - nivel 12

---
+++ 2-Exercicios.cs
@@ -0,0 +1,21 @@
+///Exercicio 1 - Nivel 2 
+// Ler dois valores inteiros para as variáveis A e B, efetuar a troca dos valores de modo que a variável A passe a
+// possuir o valor da variável B, e a variável B passe a possuir o valor da variável A. Apresentar os valores trocados. 
+// Console.WriteLine("Escreva o valor de A");
+// int A = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Escreva o valor de B");
+// int B = int.Parse(Console.ReadLine());
+
+// int C = A;
+
+// A = B;
+
+// B = C;
+
+// Console.WriteLine($"O Valor de A é igual :{A}\nO Valor de B é igual a:{B}");
+
+///Exercicio 2 - Escreva um programa que leia um número inteiro e exiba o seu módulo.
+// O módulo de um número x é:
+// x se x é maior ou igual a zero
+// x * (-1) se x é menor que zero

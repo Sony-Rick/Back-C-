4045fbbaaf118a2cc1dfb830519f3c221acba383
Erickssom Melgaco de Oliveira <erickssommelgaco@gmail.com>
2024-07-05 17:13:49 -0300

    Exercicios 11 e 12

--- 2-Exercicios.cs
+++ 2-Exercicios.cs
@@ -1,4 +1,4 @@
-///Exercicio 1 - Nivel 2 
+///Exercicio 11 - Nivel 2 
 // Ler dois valores inteiros para as variáveis A e B, efetuar a troca dos valores de modo que a variável A passe a
 // possuir o valor da variável B, e a variável B passe a possuir o valor da variável A. Apresentar os valores trocados. 
 // Console.WriteLine("Escreva o valor de A");
@@ -15,7 +15,23 @@
 
 // Console.WriteLine($"O Valor de A é igual :{A}\nO Valor de B é igual a:{B}");
 
-///Exercicio 2 - Escreva um programa que leia um número inteiro e exiba o seu módulo.
+///Exercicio 12 - Escreva um programa que leia um número inteiro e exiba o seu módulo.
 // O módulo de um número x é:
 // x se x é maior ou igual a zero
 // x * (-1) se x é menor que zero
+
+// Console.WriteLine("Digite o valor de X");
+
+// int x = int.Parse(Console.ReadLine());
+
+// if (x > 0)
+// {
+//     Console.WriteLine($"O módulo de X é igual a:{x}");
+// }
+// else if (x < 0)
+// {
+//     int modulo = x * (-1);
+//     Console.WriteLine($"O módulo de X é igual a:{modulo}");
+// }
+
+

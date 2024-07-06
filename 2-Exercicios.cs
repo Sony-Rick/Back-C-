80b134362b1d32f3c9e99e5db150b557344a00ce
Erickssom Melgaco de Oliveira <erickssommelgaco@gmail.com>
2024-07-05 23:37:00 -0300

    Exercicio !3 verificar erro

--- 2-Exercicios.cs
+++ 2-Exercicios.cs
@@ -1,8 +1,4 @@
 ///Exercicio 11 - Nivel 2 
-// Ler dois valores inteiros para as variáveis A e B, efetuar a troca dos valores de modo que a variável A passe a
-// possuir o valor da variável B, e a variável B passe a possuir o valor da variável A. Apresentar os valores trocados. 
-// Console.WriteLine("Escreva o valor de A");
-// int A = int.Parse(Console.ReadLine());
 
 // Console.WriteLine("Escreva o valor de B");
 // int B = int.Parse(Console.ReadLine());
@@ -15,10 +11,7 @@
 
 // Console.WriteLine($"O Valor de A é igual :{A}\nO Valor de B é igual a:{B}");
 
-///Exercicio 12 - Escreva um programa que leia um número inteiro e exiba o seu módulo.
-// O módulo de um número x é:
-// x se x é maior ou igual a zero
-// x * (-1) se x é menor que zero
+///Exercicio 12 - 
 
 // Console.WriteLine("Digite o valor de X");
 
@@ -33,5 +26,66 @@
 //     int modulo = x * (-1);
 //     Console.WriteLine($"O módulo de X é igual a:{modulo}");
 // }
+
+///Exercicio 13 -  Escreva um programa que leia 3 números inteiros e imprima na tela os valores em ordem decrescente.
+///Voltar depois 
+
+Console.WriteLine("Digite um valor");
+
+int numero1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite um valor");
+
+int numero2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite um valor");
+
+int numero3 = int.Parse(Console.ReadLine());
+
+if (numero1 >= numero2 && numero1 >= numero3)
+{
+    maior = numero1;
+    if (numero2 >= numero3)
+    {
+        medio = numero2;
+        menor = numero3;
+    }
+    else
+    {
+        medio = numero3;
+        menor = numero2;
+    }
+    Console.WriteLine($"A ordem dos valores em decresente : {menor} , {medio} , {maior}");
+}
+else if (numero2 >= numero1 && numero2 >= numero3)
+{
+    maior = numero2;
+    if (numero1 >= numero3)
+    {
+        medio = numero1;
+        menor = numero3;
+    }
+    else
+    {
+        medio = numero3;
+        menor = numero1;
+    }
+    Console.WriteLine($"A ordem dos valores em decresente : {menor} , {medio} , {maior}");
+}
+else
+{
+    maior = numero3;
+    if (numero1 >= numero2)
+    {
+        medio = numero1;
+        menor = numero2;
+    }
+    else
+    {
+        medio = numero2;
+        menor = numero1;
+    }
+    Console.WriteLine($"A ordem dos valores em decresente : {menor} , {medio} , {maior}");
+}
 
 

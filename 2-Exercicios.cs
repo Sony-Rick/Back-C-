5022274b164b98f438482bdced5160c5f0b82e49
Erickssom Melgaco de Oliveira <erickssommelgaco@gmail.com>
2024-07-06 23:27:55 -0300

    Exercicios 18 e 19

--- 2-Exercicios.cs
+++ 2-Exercicios.cs
@@ -220,23 +220,37 @@
 //     break;
 // }
 
-///Exercicio 19-Escreva um programa que leia três valores para os lados de um triângulo (variáveis A, B e C). Verificar se cada
-// lado é menor que a soma dos outros dois lados. Se sim, saber de A==B e se B==C, sendo verdade o triângulo é
-// eqüilátero; Se não, verificar de A==B ou se A==C ou se B==C, sendo verdade o triângulo é isósceles; e caso
-// contrário, o triângulo será escaleno. Caso os lados fornecidos não caracterizarem um triângulo, avisar a
-// ocorrência.
+///Exercicio 19
 
-Console.WriteLine("Valor de A");
+// Console.WriteLine("Valor de A");
 
-int A = int.Parse(Console.ReadLine());
+// int A = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Valor de B");
+// Console.WriteLine("Valor de B");
 
-int B = int.Parse(Console.ReadLine());
+// int B = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Valor de C");
+// Console.WriteLine("Valor de C");
 
-int C = int.Parse(Console.ReadLine());
+// int C = int.Parse(Console.ReadLine());
 
-
+// if (A < B + C && B < A + C && C < A + B)
+// {
+//     if(A == B && B == C )    
+//     {
+//         Console.WriteLine("O triàngulo é equilátero");
+//     }
+//     else if (A == B || A == C || B == C)
+//     {
+//         Console.WriteLine("O triàngulo é isósceles");
+//     }
+//     else 
+//     {
+//         Console.WriteLine("Triângulo escaleno");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("Os valores fornecidos não formam um triângulo");
+// }
 
